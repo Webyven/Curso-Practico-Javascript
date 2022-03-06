93009317f713ce823ac19502744de5a3e476812f
Weby <nicoyurec@gmail.com>
2022-03-05 21:08:45 -0300

    Nuevo taller, primera clase, creando función para calcular descuentos

---
+++ descuentos.js
@@ -0,0 +1,8 @@
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajeConDescuento = 100 - descuento;
+    const precioConDescuento = precio * porcentajeConDescuento / 100;
+
+    console.log({
+        precio, descuento, porcentajeConDescuento, precioConDescuento
+    });
+}
